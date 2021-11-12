ea310a5dd2a1dcdd5f1fbcfeb3827f534d8e6132
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-11-12 11:08:44 +0100

    kalkulator mk1

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -7,19 +7,34 @@ namespace kalkulator
         static void Main(string[] args)
         {
             Console.WriteLine("Enter first value:");
-            int number1 = int.Parse(Console.ReadLine();
+            int number1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter second value:");
-            int number2 = int.Parse(Console.ReadLine();
+            int number2 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter symbol:");
-            int symbol = int.Parse(Console.ReadLine();
+            char symbol = char.Parse(Console.ReadLine());
 
             int plus = number1 + number2;
+            int minus = number1 - number2;
+            int multiplication = number1 * number2;
+            int division = number1 / number2;
 
             if (symbol == '+')
             {
-                Console.WriteLine(plus);
+                Console.WriteLine("Your result: "+ plus);
+            }
+            else if(symbol =='-')
+            {
+                Console.WriteLine("Your result: " + minus);
+            }
+            else if (symbol == '*')
+            {
+                Console.WriteLine("Your result: " + multiplication);
+            }
+            else if (symbol == '/')
+            {
+                Console.WriteLine("Your result: " + division);
             }
         }
     }
