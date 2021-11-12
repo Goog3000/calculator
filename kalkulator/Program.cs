37474d1f794c66fdf5e4fb00d4b822b4254a678f
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-11-12 10:57:15 +0100

    Add project files.

---
+++ kalkulator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first value:");
+            int number1 = int.Parse(Console.ReadLine();
+
+            Console.WriteLine("Enter second value:");
+            int number2 = int.Parse(Console.ReadLine();
+
+            Console.WriteLine("Enter symbol:");
+            int symbol = int.Parse(Console.ReadLine();
+
+            int plus = number1 + number2;
+
+            if (symbol == '+')
+            {
+                Console.WriteLine(plus);
+            }
+        }
+    }
+}
